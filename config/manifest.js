6bac1f0a51b214155ac1065afa7685f25887d816
Chris Thoburn <cthoburn@linkedin.com>
2018-12-13 02:19:16 -0800

    make offline

---
+++ config/manifest.js
@@ -0,0 +1,34 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "guest-list",
+    short_name: "guest-list",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#fff'
+    },
+    icons: [
+      {
+        src: "/guest-list/images/icon-512.png",
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "/guest-list/images/icon-192.png",
+        sizes: "192x192",
+        type: "image/png"
+      }
+    ]
+  };
+}

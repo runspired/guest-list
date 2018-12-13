a3003b41abeb0a1ec5370d897489f53a8130d486
Chris Thoburn <cthoburn@linkedin.com>
2018-12-13 03:04:21 -0800

    wat

--- config/environment.js
+++ config/environment.js
@@ -54,7 +54,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/guest-list';
+    ENV.rootURL = '/guest-list/';
     ENV.locationType = 'hash';
   }
 
